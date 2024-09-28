8243c02aa70173a4e54785afa04093d8b00bd3b8
Alexander Steinhauer <alexander.steinhauer@tum.de>
2024-09-29 01:57:39 +0200

    add syntax highlighting and theming

--- src/lib/components/editor.svelte
+++ src/lib/components/editor.svelte
@@ -1,6 +1,8 @@
 <script>
   import { cn } from "$lib/utils";
   import * as ace from "ace-builds";
+  import "ace-builds/src-noconflict/theme-monokai";
+  import "ace-builds/src-noconflict/mode-sql";
   import { onDestroy, onMount } from "svelte";
 
   /** @type string */
@@ -13,6 +15,11 @@
 
   onMount(() => {
     editor = ace.edit(editorDiv);
+    editor.setTheme("ace/theme/monokai");
+    editor.setOption("minLines", 20);
+    editor.setOption("maxLines", 30);
+    const SqlMode = ace.require("ace/mode/sql").Mode;
+    editor.session.setMode(new SqlMode());
     editor.session.setValue(text);
     editor.session.on("change", () => {
       text = editor.session.getValue();
