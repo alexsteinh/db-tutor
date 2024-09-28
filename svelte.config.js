113f245f0b2c4daf6a180aa53cc3e82b3628b29c
Alexander Steinhauer <alexander.steinhauer@tum.de>
2024-09-29 00:42:09 +0200

    build: cloudflare deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,14 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-cloudflare";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter({
-      // adapter options go here
+      routes: {
+        include: ["/*"],
+        exclude: ["<all>"],
+      },
     }),
   },
   preprocess: vitePreprocess(),
